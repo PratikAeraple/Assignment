deb6daafb75feccf9e41e02e60fd8921a3adb88c
Pratik Aeraple <50113335+PratikAeraple@users.noreply.github.com>
2024-09-17 10:24:25 -0400

    Update index.php

--- CLOD2005/assignment1/index.php
+++ CLOD2005/assignment1/index.php
@@ -4,14 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Group F - Assignment Title</title>
+    <title>Group F - In-Class Lab-1</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <header>
         <div class="header-container">
             <h1>Welcome to Group F's Assignment</h1>
-            <h2>Assignment Title: [Your Assignment Title Here]</h2>
+            <h2>Assignment Title: In-Class Lab-1</h2>
         </div>
     </header>
     
@@ -19,14 +19,6 @@
         <section class="about">
             <h3>About Group F</h3>
             <p>We are Group F, dedicated to showcasing our skills and knowledge through this assignment.</p>
-        </section>
-        <section class="gallery">
-            <h3>Gallery</h3>
-            <div class="gallery-container">
-                <img src="images/graphic1.jpg" alt="Graphic 1">
-                <img src="images/graphic2.jpg" alt="Graphic 2">
-                <img src="images/graphic3.jpg" alt="Graphic 3">
-            </div>
         </section>
     </main>
 
